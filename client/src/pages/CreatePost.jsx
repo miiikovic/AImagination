5326df5edffb2324c9fa7ee0c244775696998200
Momcilo Mikovic <miiikovic19@gmail.com>
2023-01-24 16:09:29 +0100

    commit after deployment

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -19,10 +19,11 @@ const CreatePost = () => {
         if (form.prompt) {
             try {
                 setGeneratingImg(true);
-                const response = await fetch('http://localhost:8080/api/v1/dalle', {
+                const response = await fetch('https://aimagination.onrender.com/api/v1/dalle', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
+                        'Access-Control-Allow-Origin': '*'
                     },
                     body: JSON.stringify({
                         prompt: form.prompt,
@@ -48,10 +49,11 @@ const CreatePost = () => {
             setLoading(true);
 
             try {
-                const response = await fetch('http://localhost:8080/api/v1/post', {
+                const response = await fetch('https://aimagination.onrender.com/api/v1/post', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
+                        'Access-Control-Allow-Origin': '*'
                     },
                     body: JSON.stringify(form),
                 });
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -25,10 +25,11 @@ const Home = () => {
     const fetchPosts = async () => {
         setLoading(true);
         try {
-            const response = await fetch('http://localhost:8080/api/v1/post', {
+            const response = await fetch('https://aimagination.onrender.com/api/v1/post', {
                 method: 'GET',
                 headers: {
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
                 },
             })
 
